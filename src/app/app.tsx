1543cd9a9916a23d1b8efc5ed636382dbaf46d3f
asdqdsa <asdqdsa@protonmail.com>
2025-08-31 00:40:59 +0400

    refactor: sort, year select, search by coutnry name

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,20 @@
-import { Layout } from './layout/Layout';
+import { DataProvider } from '@/shared/model/context/DataProvider';
+import { StateProvider } from '@/shared/model/context/StateProvider';
 import { ThemeProvider } from '@/shared/model/context/ThemeProvider';
+import { Suspense } from 'react';
+import { Layout } from './layout/Layout';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Layout />
-    </ThemeProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ThemeProvider>
+        <DataProvider>
+          <StateProvider>
+            <Layout />
+          </StateProvider>
+        </DataProvider>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
--- src/features/Dashboard.tsx
+++ src/features/Dashboard.tsx
@@ -1,100 +1,127 @@
 import { cn } from '@/shared/lib/cn';
+// import { useDataContext } from '@/shared/model/context/use-data-ctx';
+import { useStateContext } from '@/shared/model/context/use-state-ctx';
+import { useCountryList } from '@/shared/model/use-country-list';
 import { UiInput } from '@/shared/uikit/ui-input';
-import {
-  memo,
-  use,
-  useCallback,
-  useDeferredValue,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
-import { fetchCO2 } from './api/fetchData';
-import type { CO2CountriesData } from './types';
-
-const defaultCountriesPromise = fetchCO2();
+import { UiSelect } from '@/shared/uikit/ui-select';
+import { memo } from 'react';
+import type { CountryData } from './types';
+import { UiButton } from '@/shared/uikit/ui-button';
 
 export function Dashboard({ className }: { className?: string }) {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [debounce, setDebounce] = useState(searchQuery);
-  const defferedQuery = useDeferredValue(debounce);
+  // const [searchQuery, setSearchQuery] = useState('');
+  // const [debounce, setDebounce] = useState(searchQuery);
+  // const defferedQuery = useDeferredValue(debounce);
+  // const data = useDataContext();
+  const countryList = useCountryList();
 
-  const [countiresPromise] = useState(defaultCountriesPromise);
-  const data = use(countiresPromise);
-
-  const onSearch = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-  }, []);
-
-  useEffect(() => {
-    const id = setTimeout(() => setDebounce(searchQuery), 400);
-    return () => clearTimeout(id);
-  }, [searchQuery]);
-
-  const filterCountries = useMemo(() => {
-    if (defferedQuery.trim() === '') return data;
-    return Object.fromEntries(
-      Object.entries(data).filter(([name, _]) => {
-        return name.toLowerCase().includes(defferedQuery.toLowerCase());
-      })
-    );
-  }, [data, defferedQuery]);
+  // useEffect(() => {
+  //   const id = setTimeout(() => setDebounce(searchQuery), 400);
+  //   return () => clearTimeout(id);
+  // }, [searchQuery]);
 
   return (
     <div className={cn('', className)}>
-      <Search value={searchQuery} onChange={onSearch} />
-      <List countries={filterCountries} />
+      <Search />
+      <YearSelect />
+      <SortByName />
+      <List countries={countryList} />
     </div>
   );
 }
 
-export function Search({
-  onChange,
-  value,
-}: {
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}) {
+export function Search() {
+  const { state, dispatch } = useStateContext();
   return (
-    <div className="py-2">
+    <div className="grid grid-cols-1 py-2">
       <UiInput
-        value={value}
+        value={state.search}
         placeholder="Type country..."
-        onChange={onChange}
-      ></UiInput>
+        className="focus:outline-none"
+        onChange={(e) =>
+          dispatch({ type: 'SEARCH', payload: { search: e.target.value } })
+        }
+      />
     </div>
+  );
+}
+
+export function YearSelect() {
+  const { state, dispatch } = useStateContext();
+
+  const years = Array.from(
+    { length: 2023 - 1750 + 1 },
+    (_, i) => 1750 + i
+  ).reverse();
+
+  return (
+    <UiSelect
+      value={state.year}
+      onChange={(e) =>
+        dispatch({
+          type: 'SET_YEAR',
+          payload: { year: Number(e.target.value) },
+        })
+      }
+    >
+      {years.map((year) => (
+        <UiSelect.Option key={year} value={year}>
+          {year}
+        </UiSelect.Option>
+      ))}
+    </UiSelect>
+  );
+}
+
+export function SortByName() {
+  const { state, dispatch } = useStateContext();
+  const currentSort = state.sort;
+  return (
+    <UiButton
+      className="p-0 font-extralight"
+      variant="outline"
+      onClick={() =>
+        dispatch({
+          type: 'SORT_NAME',
+          payload: { sort: currentSort === 'ASC' ? 'DSC' : 'ASC' },
+        })
+      }
+    >
+      {currentSort === 'ASC' ? 'Descending By Name' : 'Ascending By Name'}
+    </UiButton>
   );
 }
 
 export const List = memo(function List({
   countries,
-  // countriesPromise,
 }: {
-  countries: CO2CountriesData;
-  // countriesPromise: Promise<CO2CountriesData>;
+  countries: [string, CountryData][];
 }) {
-  // const data = use(countriesPromise);
   const data = countries;
+  const record = useStateContext().state;
+  const selectedYear = record.year;
   return (
     <div className="grid grid-cols-1">
-      <div className="bg-secondary grid grid-cols-5">
+      <div className="bg-secondary grid grid-cols-6">
         <div>Country</div>
         <div>CO2</div>
         <div>Year</div>
         <div>Population</div>
+        <div>CO2 per Capita</div>
         <div>Country Code</div>
       </div>
-      {Object.entries(data ?? []).map(([country, { data, iso_code }]) => {
-        const lastYear = data[data.length - 1]?.year ?? 'N/A';
+      {data.map(([country, { data: stats, iso_code }]) => {
+        const record = stats.find((d) => d.year === selectedYear);
         return (
           <div
-            key={iso_code ?? country}
-            className="bg-card border-border grid grid-cols-5 border p-1 shadow-md"
+            key={`${iso_code ?? country}-${selectedYear}`}
+            className="flash-bg bg-card border-border grid grid-cols-6 border p-1 shadow-md"
           >
             <div>{country}</div>
-            <div>{data[0]?.co2 ?? 'N/A'}</div>
-            <div>{lastYear}</div>
-            <div>{data[0]?.population ?? 'N/A'}</div>
+            <div>{record?.co2 ?? 'N/A'}</div>
+            <div>{selectedYear}</div>
+            <div>{record?.population ?? 'N/A'}</div>
+            <div>{record?.co2_per_capita ?? 'N/A'}</div>
             <div>{iso_code ?? 'N/A'}</div>
           </div>
         );
